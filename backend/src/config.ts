7969bf0022a3a4c2598e345c7cea73b0d1769cc6
Clément Fassot <kedketh@gmail.com>
2019-10-06 16:59:51 +0200

    :construction_worker: Fix ci (#17)

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,39 +1,38 @@
 export type Config = {
-    port: number;
-    playground?: boolean;
-    introspection?: boolean;
-    adminToken?: string;
-  };
-  
-  const envVariablesList = [
-    { name: "PORT", type: Number, optional: false },
-    { name: "PLAYGROUND", type: Boolean, optional: true },
-    { name: "INTROSPECTION", type: Boolean, optional: true },
-    { name: "ADMIN_TOKEN", type: String, optional: true }
-  ];
-  
-  const buildConfigObject = (): Config => {
-    const errors: string[] = [];
-    const config: Config = envVariablesList
-      .map(row => {
-        if (
-          !row.optional &&
-          (!process.env[row.name] || !row.type(process.env[row.name]))
-        ) {
-          errors.push(row.name);
-        }
-        return { [row.name.toLowerCase()]: row.type(process.env[row.name]) };
-      })
-      .reduce((previous, current) => ({ ...previous, ...current })) as Config;
-  
-    if (errors.length > 0) {
-      throw new Error(
-        `ERROR: env variable(s) [ ${errors.join(", ")} ] has/have not been set`
-      );
-    }
-  
-    return config;
-  };
-  
-  export const config = buildConfigObject();
-  
+  port: number;
+  playground?: boolean;
+  introspection?: boolean;
+  adminToken?: string;
+};
+
+const envVariablesList = [
+  { name: "PORT", type: Number, optional: false },
+  { name: "PLAYGROUND", type: Boolean, optional: true },
+  { name: "INTROSPECTION", type: Boolean, optional: true },
+  { name: "ADMIN_TOKEN", type: String, optional: true }
+];
+
+const buildConfigObject = (): Config => {
+  const errors: string[] = [];
+  const config: Config = envVariablesList
+    .map(row => {
+      if (
+        !row.optional &&
+        (!process.env[row.name] || !row.type(process.env[row.name]))
+      ) {
+        errors.push(row.name);
+      }
+      return { [row.name.toLowerCase()]: row.type(process.env[row.name]) };
+    })
+    .reduce((previous, current) => ({ ...previous, ...current })) as Config;
+
+  if (errors.length > 0) {
+    throw new Error(
+      `ERROR: env variable(s) [ ${errors.join(", ")} ] has/have not been set`
+    );
+  }
+
+  return config;
+};
+
+export const config = buildConfigObject();
