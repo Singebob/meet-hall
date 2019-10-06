e1174aaaf3c11cd41cf6954efc6f8635495b29df
Clément Fassot <kedketh@gmail.com>
2019-10-06 14:41:24 +0200

    :sparkles: Add backend config (#13)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
 // Example koa server with postgraphile here: https://github.com/graphile/examples/tree/master/server-koa2 , this still needs completion
 import Koa from "koa";
 import { AddressInfo } from "net";
+import { config } from "./config";
 
 const getServerPort = (address: string | AddressInfo | null) =>
   address && typeof address !== "string"
     ? address.port
-    : new Error("Oops, we couldn't find the port");
+    : new Error("Error, couldn't find the port");
 
 const app = new Koa();
-const server = app.listen(3000); // use env variables later
+const server = app.listen(config.port);
 const address = server.address();
 console.log(`Server succesfuly started on port ${getServerPort(address)}`);
