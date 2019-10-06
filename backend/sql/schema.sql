16de15f8dd817c761e036505050f7647b6ca2b91
Maël <shyndard@gmail.com>
2019-10-06 21:50:46 +0200

    Fix column name (better comprehension) (#19)

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,109 +1,109 @@
 CREATE TABLE organization (
-	org_uuid UUID primary key,
-	org_name VARCHAR(50),
-	org_description VARCHAR(200)
+	"uuid" UUID primary key,
+	"name" VARCHAR(50),
+	"description" VARCHAR(200)
 );
 
 CREATE TABLE "user" (
-	use_uuid UUID primary key,
-	use_displayname VARCHAR(50),
-	use_firstname VARCHAR(50),
-	use_lastname VARCHAR(50),
-	use_email VARCHAR(50)
+	"uuid" UUID primary key,
+	"displayname" VARCHAR(50),
+	"firstname" VARCHAR(50),
+	"lastname" VARCHAR(50),
+	"email" VARCHAR(50)
 );
 
 CREATE TABLE "event" (
-	eve_uuid UUID primary key,
-	eve_name VARCHAR(50),
-	eve_description VARCHAR(2000),
-	eve_date TIMESTAMP,
-	eve_location VARCHAR(100),
-	eve_arviched boolean default false
+	"uuid" UUID primary key,
+	"name" VARCHAR(50),
+	"description" VARCHAR(2000),
+	"date" TIMESTAMP,
+	"location" VARCHAR(100),
+	"arviched" boolean default false
 );
 
 CREATE TABLE "role" (
-	rol_uuid UUID primary key,
-	rol_name VARCHAR(50)
+	"uuid" UUID primary key,
+	"name" VARCHAR(50)
 );
 
 CREATE TABLE tag_category (
-	tca_uuid UUID primary key,
-	tca_name VARCHAR(30),
-	tca_color VARCHAR(10)
+	"uuid" UUID primary key,
+	"name" VARCHAR(30),
+	"color" VARCHAR(10)
 );
 
 CREATE TABLE entity_type (
-	ety_uuid UUID primary key,
-	ety_name VARCHAR(20)
+	"uuid" UUID primary key,
+	"name" VARCHAR(20)
 );
 
 CREATE TABLE user_organization (
-	org_uuid UUID,
-	use_uuid UUID,
-	CONSTRAINT PK_user_organization PRIMARY KEY (org_uuid, use_uuid),
-	FOREIGN KEY (org_uuid) REFERENCES organization (org_uuid),
-	FOREIGN KEY (use_uuid) REFERENCES "user" (use_uuid)
+	"organization_uuid" UUID,
+	"user_uuid" UUID,
+	CONSTRAINT PK_user_organization PRIMARY KEY (organization_uuid, user_uuid),
+	FOREIGN KEY (organization_uuid) REFERENCES organization (uuid),
+	FOREIGN KEY (user_uuid) REFERENCES "user" (uuid)
 );
 
 CREATE TABLE event_request (
-	ere_uuid UUID primary key,
-	use_uuid UUID,
-	ere_name VARCHAR(50),
-	eve_description VARCHAR(2000),
-	FOREIGN KEY (use_uuid) REFERENCES "user" (use_uuid)
+	"uuid" UUID primary key,
+	"creator" UUID,
+	name VARCHAR(50),
+	description VARCHAR(2000),
+	FOREIGN KEY (creator) REFERENCES "user" (uuid)
 );
 
 CREATE TABLE user_event_participation (
-	use_uuid UUID,
-	eve_uuid UUID,
-	rol_uuid UUID,
-	CONSTRAINT PK_user_event_participation PRIMARY KEY (use_uuid, eve_uuid, rol_uuid),
-	FOREIGN KEY (use_uuid) REFERENCES "user" (use_uuid),
-	FOREIGN KEY (eve_uuid) REFERENCES "event" (eve_uuid),
-	FOREIGN KEY (rol_uuid) REFERENCES "role" (rol_uuid)
+	"user_uuid" UUID,
+	"event_uuid" UUID,
+	"role_uuid" UUID,
+	CONSTRAINT PK_user_event_participation PRIMARY KEY (user_uuid, event_uuid, role_uuid),
+	FOREIGN KEY (user_uuid) REFERENCES "user" (uuid),
+	FOREIGN KEY (event_uuid) REFERENCES "event" (uuid),
+	FOREIGN KEY (role_uuid) REFERENCES "role" (uuid)
 );
 
 CREATE TABLE tag (
-	tag_uuid UUID PRIMARY KEY,
-	tag_name VARCHAR(20),
-	tca_uuid UUID,
-	FOREIGN KEY (tca_uuid) REFERENCES tag_category (tca_uuid)
+	"uuid" UUID PRIMARY KEY,
+	"name" VARCHAR(20),
+	"category" UUID,
+	FOREIGN KEY (category) REFERENCES tag_category (uuid)
 );
 
 CREATE TABLE available_tag_category (
-	tca_uuid UUID,
-	ety_uuid UUID,
-	CONSTRAINT PK_available_tag_category PRIMARY KEY (tca_uuid, ety_uuid),
-	FOREIGN KEY (tca_uuid) REFERENCES tag_category (tca_uuid),
-	FOREIGN KEY (ety_uuid) REFERENCES entity_type (ety_uuid)
+	"tag_category_uuid" UUID,
+	"entity_type_uuid" UUID,
+	CONSTRAINT PK_available_tag_category PRIMARY KEY (tag_category_uuid, entity_type_uuid),
+	FOREIGN KEY (tag_category_uuid) REFERENCES tag_category (uuid),
+	FOREIGN KEY (entity_type_uuid) REFERENCES entity_type (uuid)
 );
 
 CREATE TABLE event_tag (
-	eve_uuid UUID,
-	tag_uuid UUID,
-	CONSTRAINT PK_event_tag PRIMARY KEY (eve_uuid, tag_uuid),
-	FOREIGN KEY (eve_uuid) REFERENCES "event" (eve_uuid),
-	FOREIGN KEY (tag_uuid) REFERENCES tag (tag_uuid)
+	"event_uuid" UUID,
+	"tag_uuid" UUID,
+	CONSTRAINT PK_event_tag PRIMARY KEY (event_uuid, tag_uuid),
+	FOREIGN KEY (event_uuid) REFERENCES "event" (uuid),
+	FOREIGN KEY (tag_uuid) REFERENCES tag (uuid)
 );
 
 CREATE TABLE organization_tag (
-	org_uuid UUID,
-	tag_uuid UUID,
-	CONSTRAINT PK_organization_tag PRIMARY KEY (org_uuid, tag_uuid),
-	FOREIGN KEY (org_uuid) REFERENCES organization (org_uuid),
-	FOREIGN KEY (tag_uuid) REFERENCES tag (tag_uuid)
+	"organization_uuid" UUID,
+	"tag_uuid" UUID,
+	CONSTRAINT PK_organization_tag PRIMARY KEY (organization_uuid, tag_uuid),
+	FOREIGN KEY (organization_uuid) REFERENCES organization (uuid),
+	FOREIGN KEY (tag_uuid) REFERENCES tag (uuid)
 );
 
 CREATE TABLE user_tag (
-	use_uuid UUID,
-	tag_uuid UUID,
-	CONSTRAINT PK_user_tag PRIMARY KEY (use_uuid, tag_uuid),
-	FOREIGN KEY (use_uuid) REFERENCES "user" (use_uuid),
-	FOREIGN KEY (tag_uuid) REFERENCES tag (tag_uuid)
+	"user_uuid" UUID,
+	"tag_uuid" UUID,
+	CONSTRAINT PK_user_tag PRIMARY KEY (user_uuid, tag_uuid),
+	FOREIGN KEY (user_uuid) REFERENCES "user" (uuid),
+	FOREIGN KEY (tag_uuid) REFERENCES tag (uuid)
 );
 
-CREATE USER postgraphile_data_management WITH PASSWORD '${postgraphile_data_management_user_password}';
-CREATE USER postgraphile_full_access WITH PASSWORD '${postgraphile_full_access_user_password}';
+CREATE USER postgraphile_data_management WITH PASSWORD '${POSTGRAPHILE_DATA_MANAGEMENT_USER_PASSWORD}';
+CREATE USER postgraphile_full_access WITH PASSWORD '${POSTGRAPHILE_FULL_ACCESS_USER_PASSWORD}';
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO postgraphile_data_management;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgraphile_full_access;
